fc4f5a155c3a4aacebcedf5e96cad37ac26df588
rkranjan7894 <rkranjan7894@gmail.com>
2024-12-03 16:35:50 +0530

    Add Initial Files

---
+++ src/test/java/com/spring/__Ecommerce/App/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.__Ecommerce.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
